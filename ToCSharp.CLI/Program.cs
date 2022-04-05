24dbdf424ff9cada7d2424a95384a79abe72f283
BAILLEUL <sacha.bailleul@neos-sdi.com>
2022-04-05 11:04:27 +0200

    [feat]: Start implementation of XmlParser

--- ToCSharp.CLI/Program.cs
+++ ToCSharp.CLI/Program.cs
@@ -1,3 +1,7 @@
-﻿using ToCSharp.CLI.CommandLine;
+﻿using System.Xml.Linq;
+using ToCSharp.CLI.CommandLine;
+using ToCSharp.Core.Parsing;
+using ToCSharp.Core.Parsing.Xml;
 
+ExtensionMatcher.SetParser(".xml", () => new XmlParser());
 await ArgumentsProcessor.ProcessAsync(args);
---
+++ ToCSharp.Core/Parsing/Xml/XmlParser.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Collections.Immutable;
+using ToCSharp.Core.Generation.Contracts;
+using ToCSharp.Core.Generation.Definitions;
+
+namespace ToCSharp.Core.Parsing.Xml;
+
+public class XmlParser: IParser
+{
+    public IImmutableSet<Class> Parse(TextReader txtReader)
+    {
+        throw new NotImplementedException();
+    }
+}
